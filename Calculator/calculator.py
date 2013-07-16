4521e1e793bde5a889dfa6d23668e2eede4a6b08
lmaxapex <86088341@qq.com>
2013-07-16 02:47:32 -0700

    calculator commit

---
+++ Calculator/calculator.py
@@ -0,0 +1,39 @@
+#Basic calculator project
+import math
+def cube(n):
+    return n**3
+
+def squareroot(n):
+    """
+    Returns the square root of the number n. If n < 0, 
+    then return the string "NAN" (Not A Number)
+    """
+    "REPLACE THIS CODE WITH YOUR SQUAREROOT METHOD"
+    if n<0:
+        return "NAN"
+    else:
+        return math.sqrt(n)
+
+def negate(n):
+    """ Return the negative value of the argument 'n'.
+    """
+    "REPLACE THIS CODE WITH YOUR NEGATE METHOD"
+    
+    return -n
+
+def factorial(n):
+    """Return n factorial
+    The factorial of anything <= 1 is 1.
+    The factorial of any integer greater than 1 is the product of all
+    the integers from 1 to the number itself. For example,
+    4 factorial = 1 x 2 x 3 x 4 = 24.
+    """
+    if n == 0:
+        return 1
+    if n < 0:
+        return "NAN"
+    else:
+        return n*factorial(n-1)
+    "REPLACE THIS CODE WITH YOUR FACTORIAL METHOD"
+    
+    
